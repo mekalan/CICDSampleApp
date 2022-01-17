1ebdea62af909bae1817fe2a64a2005439e3af12
NandyalaSanthi <97028263+NandyalaSanthi@users.noreply.github.com>
2022-01-17 17:13:36 +0530

    test2

--- app/src/main/java/com/example/cicdsampleapp/MainActivity.kt
+++ app/src/main/java/com/example/cicdsampleapp/MainActivity.kt
@@ -8,6 +8,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //test comments hjsfgfrgjlkfjn skdjhfkjsdfnfjksdnkj bakjsbfkjesdbfkjsdbfkj
-       // dshhhjjhkl;'sdhjfnkdjfnfkjsnfkjsnbkjnskjgnfrsjkgnjkfsngkjfngkjv nejfbkshjdbfhjsb d
     }
 }
